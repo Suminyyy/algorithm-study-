4a1ec01441f0764156675b9899b236e752d336ed
Suminyyy <sumin908@naver.com>
2022-08-25 18:44:09 +0900

    Codeup6070문제

---
+++ Codeup6070.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a == 12 or a == 1 or a == 2:
+    print("winter")
+else:
+    if a == 3 or a == 4 or a == 5:
+        print("spring")
+    else:
+        if a == 6 or a == 7 or a == 8:
+            print("summer")
+        else:
+            print("fall")
